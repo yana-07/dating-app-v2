5131eea7f2cf3d223cb922e0a9c1b707f2a78735
yana.radeva7 <yana.radeva7@gmail.com>
2025-07-21 19:53:08 +0300

    Add first entity, db context and initial migration

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities;
+
+public class AppUser
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+
+    public required string DisplayName { get; set; }
+
+    public required string Email { get; set; }
+}
