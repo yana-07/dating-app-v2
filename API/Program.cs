702fa15a128781f594fd642ebd00e4c021ceeb76
yana.radeva7 <yana.radeva7@gmail.com>
2025-07-23 11:22:44 +0300

    Add simple MembersController and set up CORS

--- API/Program.cs
+++ API/Program.cs
@@ -5,15 +5,27 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddDbContext<AppDbContext>(options => 
-    options.UseSqlite(
-        builder.Configuration.GetConnectionString("DefaultConnection")
-    )
-);
+builder.Services.AddDbContext<AppDbContext>(optionsBuilder =>
+{
+    var connectionString = builder.Configuration
+        .GetConnectionString("DefaultConnection");
+    optionsBuilder.UseSqlite(connectionString);
+});
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AngularApp", policyBuilder =>
+    {
+        policyBuilder
+            .WithOrigins("http://localhost:4200", "https://localhost:4200")
+            .AllowAnyHeader().AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors("AngularApp");
+
 app.MapControllers();
 
 app.Run();
