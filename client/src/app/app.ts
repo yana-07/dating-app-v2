11eea31bb5d3014ad6aff76d28b3227ce2a8fb51
yana.radeva7 <yana.radeva7@gmail.com>
2025-07-23 13:12:00 +0300

    Fetch and display all members

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,10 +10,11 @@ import { Observable } from 'rxjs';
 export class App implements OnInit {
   private http = inject(HttpClient);
   protected readonly title = signal('Dating App');
+  protected members = signal<any>([]);
 
   ngOnInit(): void {
     this.http.get('https://localhost:5001/api/members').subscribe({
-      next: response => console.log(response),
+      next: response => this.members.set(response),
       error: error => console.log(error),
       complete: () => console.log('Completed the http request.')
     });
