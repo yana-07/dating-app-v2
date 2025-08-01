4b0cd8fe0ab1cad64b7974a4bd17065b717c42bd
yana.radeva7 <yana.radeva7@gmail.com>
2025-08-01 13:33:34 +0300

    Clean up app component; Enable view transitions

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
@@ -9,7 +9,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), 
+    provideRouter(routes, withViewTransitions()), 
     provideHttpClient(),
     provideAppInitializer(() => {
       const accountService = inject(AccountService);
--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,10 +1,7 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Router, RouterOutlet } from '@angular/router';
 
 import { Nav } from "../layout/nav/nav";
-import { AccountService } from '../core/services/account-service';
-import { User } from '../types/user';
 
 @Component({
   selector: 'app-root',
@@ -12,17 +9,6 @@ import { User } from '../types/user';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit {
-  private http = inject(HttpClient);
+export class App {
   protected router = inject(Router);
-  protected readonly title = signal('Dating App');
-  protected members = signal<User[]>([]);
-
-  ngOnInit(): void {
-    this.http.get<User[]>('https://localhost:5001/api/members').subscribe({
-      next: response => this.members.set(response),
-      error: error => console.log(error),
-      complete: () => console.log('Completed the http request.')
-    });
-  }
 }
