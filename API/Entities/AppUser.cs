ff777f502ee5068d1c4d5ec08d41f403f1675dbb
yana.radeva7 <yana.radeva7@gmail.com>
2025-08-06 15:30:53 +0300

    Add Member and Photo entities; Configure relationships and navigation properties

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -8,7 +8,11 @@ public class AppUser
 
     public required string Email { get; set; }
 
+    public string? ImageUrl { get; set; }
+
     public required byte[] PasswordHash { get; set; }
 
     public required byte[] PasswordSalt { get; set; }
+
+    public Member Member { get; set; } = null!;
 }
---
+++ API/Entities/Photo.cs
@@ -0,0 +1,14 @@
+﻿namespace API.Entities;
+
+public class Photo
+{
+    public int Id { get; set; }
+
+    public required string Url { get; set; }
+
+    public string? PublicId { get; set; }
+
+    public string MemberId { get; set; } = null!;
+
+    public Member Member { get; set; } = null!;
+}
