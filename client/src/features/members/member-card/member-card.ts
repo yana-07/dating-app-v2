12c4511dc7b58158f6b4a5604f6589a740e87c80
yana.radeva7 <yana.radeva7@gmail.com>
2025-08-07 17:47:37 +0300

    Add member-card component; Get and display members

---
+++ client/src/features/members/member-card/member-card.ts
@@ -0,0 +1,13 @@
+import { Component, input } from '@angular/core';
+
+import { Member } from '../../../types/member';
+
+@Component({
+  selector: 'app-member-card',
+  imports: [],
+  templateUrl: './member-card.html',
+  styleUrl: './member-card.css'
+})
+export class MemberCard {
+  member = input.required<Member>();
+}
--- client/src/features/members/member-list/member-list.ts
+++ client/src/features/members/member-list/member-list.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
+
+import { MemberService } from '../../../core/services/member-service';
+import { Member } from '../../../types/member';
+import { MemberCard } from "../member-card/member-card";
 
 @Component({
   selector: 'app-member-list',
-  imports: [],
+  imports: [AsyncPipe, MemberCard],
   templateUrl: './member-list.html',
   styleUrl: './member-list.css'
 })
-export class MemberList {
+export class MemberList implements OnInit {
+  private memberService = inject(MemberService);
+  protected members$!: Observable<Member[]>;
 
+  ngOnInit(): void {
+    this.members$ = this.memberService.getMembers();
+  }
 }
