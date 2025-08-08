c76fe00b29fe4ebecaeb65ab30a8ac053764ebf4
yana.radeva7 <yana.radeva7@gmail.com>
2025-08-08 11:10:04 +0300

    Add interceptor to set Authroization header with jwt

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -11,13 +11,14 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { AccountService } from '../core/services/account-service';
 import { errorInterceptor } from '../core/interceptors/error-interceptor';
+import { jwtInterceptor } from '../core/interceptors/jwt-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes, withViewTransitions()),
-    provideHttpClient(withInterceptors([errorInterceptor])),
+    provideHttpClient(withInterceptors([errorInterceptor, jwtInterceptor])),
     provideAppInitializer(() => {
       const accountService = inject(AccountService);
 
