519e5d1ad03eaf2a2f2b06d110ecd89a41f1eec9
yana.radeva7 <yana.radeva7@gmail.com>
2025-08-08 10:45:30 +0300

    Style the member cards

--- client/src/features/members/member-card/member-card.ts
+++ client/src/features/members/member-card/member-card.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 import { Member } from '../../../types/member';
 
 @Component({
   selector: 'app-member-card',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './member-card.html',
   styleUrl: './member-card.css'
 })
