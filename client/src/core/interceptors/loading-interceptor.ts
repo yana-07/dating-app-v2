281b3880a829df7bce1f767616a7bc892766e542
yana.radeva7 <yana.radeva7@gmail.com>
2025-08-14 13:30:21 +0300

    Remove caching to prevent stale views

--- client/src/core/interceptors/loading-interceptor.ts
+++ client/src/core/interceptors/loading-interceptor.ts
@@ -1,26 +1,16 @@
-import { HttpEvent, HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { delay, finalize, of, tap } from 'rxjs';
+import { delay, finalize } from 'rxjs';
 
 import { BusyService } from '../services/busy-service';
 
-const cache = new Map<string, HttpEvent<unknown>>();
-
 export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
   const busyService = inject(BusyService);
-
-  if (req.method === 'GET') {
-    const cachedResponse = cache.get(req.url);
-    if (cachedResponse) {
-      return of(cachedResponse);
-    }
-  }
 
   busyService.busy();
 
   return next(req).pipe(
     delay(500),
-    tap(response => cache.set(req.url, response)),
     finalize(() => busyService.idle())
   );
 };
