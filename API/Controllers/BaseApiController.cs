8d1acc70719d8decd6de69b9a806fa6612c1c501
yana.radeva7 <yana.radeva7@gmail.com>
2025-08-22 16:25:12 +0300

    Add action filter to update member lastActive property after each authenticated request

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using API.Helpers;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
 [Route("api/[controller]")]
+[ServiceFilter(typeof(LogUserActivity))]
 [ApiController]
 public class BaseApiController : ControllerBase
 {
---
+++ API/Helpers/LogUserActivity.cs
@@ -0,0 +1,24 @@
+﻿using API.Data;
+using API.Extensions;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Helpers;
+
+public class LogUserActivity(AppDbContext dbContext) : IAsyncActionFilter
+{
+    public async Task OnActionExecutionAsync(
+        ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        var resultContex = await next();
+
+        if (context.HttpContext.User.Identity?.IsAuthenticated != true) return;
+
+        var memberId = resultContex.HttpContext.User.GetMemberId();
+
+        await dbContext.Members
+            .Where(member => member.Id == memberId)
+            .ExecuteUpdateAsync(setters => 
+                setters.SetProperty(member => member.LastActive, DateTime.UtcNow));
+    }
+}
