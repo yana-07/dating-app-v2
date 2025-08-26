16661ac843c2dd673cf2c5774c27a0d764080942
yana.radeva7 <yana.radeva7@gmail.com>
2025-08-26 18:16:16 +0300

    Implement ListsComponent with tabs (Liked, Liked me, Mutual likes)

--- client/src/features/lists/lists.ts
+++ client/src/features/lists/lists.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
+
+import { LikeService } from '../../core/services/like-service';
+import { Member } from '../../types/member';
+import { MemberCard } from "../members/member-card/member-card";
 
 @Component({
   selector: 'app-lists',
-  imports: [],
+  imports: [MemberCard],
   templateUrl: './lists.html',
   styleUrl: './lists.css'
 })
 export class Lists {
+  private likeService = inject(LikeService);
+  protected predicate = signal('liked');
+  protected members = signal<Member[]>([]);
+  protected tabs = [
+    { label: 'Liked', value: 'liked' },
+    { label: 'Liked me', value: 'likedBy' },
+    { label: 'Mutual', value: 'mutual' }
+  ];
 
+  constructor() {
+    effect(() => {
+      this.loadLikes();
+    });  
+  }
+
+  loadLikes() {
+    this.likeService.getLikes(this.predicate()).subscribe({
+      next: members => this.members.set(members)  
+    });
+  }
+
+  setPredicate(predicate: string) {
+    if (predicate !== this.predicate()) {
+      this.predicate.set(predicate);
+    }
+  }
 }
