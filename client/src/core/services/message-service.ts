0d8fc65276cf30916de6eaf93eaa5341f611e76a
yana.radeva7 <yana.radeva7@gmail.com>
2025-09-03 13:32:14 +0300

    Add client-side fetching and display of inbox/outbox messages

---
+++ client/src/core/services/message-service.ts
@@ -0,0 +1,24 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+import { environment } from '../../environments/environment';
+import { PaginatedResult, PagingParams } from '../../types/pagination';
+import { Message } from '../../types/message';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+  private baseUrl = environment.apiUrl;
+  private http = inject(HttpClient);
+
+  getMessages(container: string, pagingParams: PagingParams) {
+    const params = new HttpParams()
+    .append('page', pagingParams.page)
+    .append('pageSize', pagingParams.pageSize)
+    .append('container', container);
+
+    return this.http.get<PaginatedResult<Message>>(
+      `${this.baseUrl}/messages`, { params });
+  }
+}
--- client/src/features/messages/messages.ts
+++ client/src/features/messages/messages.ts
@@ -1,11 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
+
+import { MessageService } from '../../core/services/message-service';
+import { PaginatedResult, PagingParams } from '../../types/pagination';
+import { Message } from '../../types/message';
+import { Paginator } from "../../shared/paginator/paginator";
 
 @Component({
   selector: 'app-messages',
-  imports: [],
+  imports: [Paginator],
   templateUrl: './messages.html',
   styleUrl: './messages.css'
 })
 export class Messages {
+  private messageService = inject(MessageService);
+  private pagingParams = signal(new PagingParams());
+  protected container = signal('Inbox');
+  protected paginatedMessages = signal<PaginatedResult<Message> | undefined>(undefined);
+  protected tabs = [
+    { label: 'Inbox', value: 'Inbox' },
+    { label: 'Outbox', value: 'Outbox' }
+  ];
 
+  constructor() {
+    effect(() => {
+      this.loadMessages();
+    });  
+  }
+
+  loadMessages() {
+    this.messageService.getMessages(this.container(), this.pagingParams()).subscribe({
+      next: paginatedResult => this.paginatedMessages.set(paginatedResult)  
+    });
+  }
+
+  onPageChange(newPage: number) {
+    this.pagingParams.update(prevParams => {
+      return prevParams ?
+        { ...prevParams, page: newPage } : 
+        prevParams;
+    });
+  }
+
+  onPageSizeChange(newPageSize: number) {
+    this.pagingParams.update(prevParams => {
+      return prevParams ? 
+        { ...prevParams, pageSize: newPageSize } : 
+        prevParams;
+    });
+  }
+
+  onContainerChange(newContainer: string) {
+    if (newContainer !== this.container()) {
+      this.container.set(newContainer);
+      this.pagingParams.update(prevParams => {
+        return prevParams ?
+          { ...prevParams, page: 1 } :
+          prevParams;
+      });
+    }
+  }
+
+  get isInbox() {
+    return this.container() === 'Inbox';
+  }
 }
