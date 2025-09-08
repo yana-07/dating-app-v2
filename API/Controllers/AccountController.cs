f48beca2be1d83b3e8666a2317ebfc4da6354d35
yana.radeva7 <yana.radeva7@gmail.com>
2025-09-08 13:07:12 +0300

    Use UserManager identity service in AccountController

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,35 +1,25 @@
-﻿using API.Data;
-using API.DTOs;
+﻿using API.DTOs;
 using API.Entities;
+using API.Enums;
 using API.Extensions;
 using API.Interfaces;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using System.Security.Cryptography;
-using System.Text;
 
 namespace API.Controllers;
 
 public class AccountController(
-    AppDbContext dbContext, ITokenService tokenService) 
+    UserManager<AppUser> userManager, ITokenService tokenService) 
     : BaseApiController
 {
     [HttpPost("register")]
     public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
     {
-        if (await EmailExists(registerDto.Email))
-        {
-            return BadRequest("Email is taken.");
-        }
-
-        using var hmac = new HMACSHA512();
-
         var user = new AppUser
         {
             DisplayName = registerDto.DisplayName,
             Email = registerDto.Email,
-            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-            PasswordSalt = hmac.Key,
+            UserName = registerDto.Email,
             Member = new Member
             {
                 DisplayName = registerDto.DisplayName,
@@ -40,42 +30,39 @@ public class AccountController(
             }
         };
 
-        dbContext.Users.Add(user);
-        await dbContext.SaveChangesAsync();
+        var result = await userManager.CreateAsync(user, registerDto.Password);
+        if (!result.Succeeded)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("identity", error.Description);
+            }
 
-        return user.ToDto(tokenService);
+            return ValidationProblem();
+        }
+
+        await userManager.AddToRoleAsync(user, nameof(UserRoles.Member));
+
+        return await user.ToDto(tokenService);
     }
 
     [HttpPost("login")]
     public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
     {
-        var user = await dbContext.Users
-            .SingleOrDefaultAsync(user =>
-                user.Email.ToLower() == loginDto.Email.ToLower());
+        var user = await userManager.FindByEmailAsync(loginDto.Email);
 
         if (user is null)
         {
             return Unauthorized("Invalid email address.");
         }
 
-        using var hmac = new HMACSHA512(user.PasswordSalt);
+        var isValidPassword = await userManager.CheckPasswordAsync(user, loginDto.Password);
 
-        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
-
-        for (int i = 0; i < computedHash.Length; i++)
+        if (!isValidPassword)
         {
-            if (computedHash[i] != user.PasswordHash[i])
-            {
-                return Unauthorized("Invalid password.");
-            }
+            return Unauthorized("Invalid password.");
         }
 
-        return user.ToDto(tokenService);
-    }
-
-    private async Task<bool> EmailExists(string email)
-    {
-        return await dbContext.Users
-            .AnyAsync(user => user.Email.ToLower() == email.ToLower());
+        return await user.ToDto(tokenService);
     }
 }
